25c5bc3eba8bf4eee72bdf25a5e8b02512314c05
kmcmurtrey <korey.mcmurtrey@gmail.com>
2016-01-14 22:45:16 -0500

    Uses empty() to check , and strpos()

--- src/string_search.php
+++ src/string_search.php
@@ -1,12 +1,22 @@
 <?php
 
 function string_search ($needle, $haystack) {
+  
+  //Check if $needle is empty, and return false if so.
+  if (empty($needle)) {
+    
+    return false;
+    
+  } else {
+    
+    //Use strpos() to search for needle, returning the position if found, and returnig false if not.
     $position = strpos ($haystack, $needle);
 
-    if ($position === false || $needle === '') {
-        return false;
-    } else {
-        return "Found '$needle' at index $position";
-    }
+    if ($position !== false) {
+      return "Found '$needle' at index $position";
+    }   
     
+    return false;
+
+  }    
 }
