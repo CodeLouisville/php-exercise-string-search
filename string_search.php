1b9d90a2c0b288ec7abc7f4db7753aedbce8ad12
Tony T <TonyTatsumaki@live.com>
2016-05-24 18:24:10 -0400

    First attempt!
    ...why do we leave needles in haystacks!?

---
+++ string_search.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+
+    <?php
+
+    $pos = strpos($haystack, $needle);
+
+    function string_search($needle, $haystack){
+        if($needle == ''){
+        return false;
+        }
+
+
+    if($pos === false){
+        return false;
+        } else {
+        return "found your" . $needle . " at " . $pos; 
+        }
+    }
+
+    ?>
+
+</body>
+</html>
