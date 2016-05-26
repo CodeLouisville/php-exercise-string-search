d3101ef7e609e78e118f41ee0b46bd53b5258eb8
maq23 <mmcconnell23@gmail.com>
2016-05-25 22:36:26 -0400

    string search file

---
+++ src/string_search.php
@@ -0,0 +1,25 @@
+<?php
+  
+  function string_search ($needle, $haystack) {
+  
+   if (empty($needle)) {
+     
+     return false;
+     
+   } else {
+     
+      $position = strpos ($haystack, $needle);
+  
+     if ($position === false || $needle === '') {
+         return false;
+     } else {
+         return "Found '$needle' at index $position";
+     }
+     if ($position !== false) {
+       return "Found '$needle' at index $position";
+     }   
+      
+     return false;
+ 
+   }    
+  } 
