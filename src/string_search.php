8e1948fb0448166170ff3d2ae1c3da9e3b836ad5
SubasiniR <subasiniit@gmail.com>
2016-05-26 12:29:50 -0400

    corrected

--- src/string_search.php
+++ src/string_search.php
@@ -7,8 +7,12 @@ function string_search($needle, $haystack){
 	} else{
 		$x = strpos($haystack, $needle);
 		echo "Found '".$needle."' at index ".$x;
-		return true;
+		//return true;
 	}
 }
 
 //var_dump(string_search("Code Louisville", "Searching Code Louisville."));
+
+//string_search("Code Louisville", "Searching Code Louisville.");
+
+//var_dump(string_search("zx", "Searching Code Louisville."));
