4eeaa999f4be178c3a756194395979285eb73ca8
Nicholas Dixon <nldixon@gmail.com>
2016-06-21 22:47:51 +0000

    Did I find the needle in the haystack?

---
+++ src/string_search.php
@@ -0,0 +1,14 @@
+<?php
+
+$pos = strpos($needle, $haystack);
+
+function string_search($needle, $haystack) {
+    if ($needle === "") {
+        return false;
+        echo "'$needle' was not found in '$haystack.'";
+    } else {
+        echo "'$needle' was found in '$haystack' at '$pos'."
+    }
+}
+
+/?>
