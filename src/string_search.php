df67c5e5efa6e058206581b1e7881455d7768cc2
Nicholas Dixon <nldixon@gmail.com>
2016-06-21 23:33:53 +0000

    Ugh.

--- src/string_search.php
+++ src/string_search.php
@@ -5,9 +5,8 @@ function string_search($needle, $haystack) {
 
     if ($needle === "") {
         return false;
-        echo "'$needle' was not found in '$haystack.'";
     } else {
-        echo "'$needle' was found in '$haystack' at '$pos'.";
+        return "Found '" . $needle . "' at index " . $pos;
     }
 }
 
