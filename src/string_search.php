40cba948ea1cdc07702cff7bbe27a641af3e7e2d
Nicholas Dixon <nldixon@gmail.com>
2016-06-21 23:41:39 +0000

    Failure.

--- src/string_search.php
+++ src/string_search.php
@@ -1,11 +1,12 @@
 <?php
 
-$pos = strpos($needle, $haystack);
-
 function string_search($needle, $haystack) {
+    
+    $pos = strpos($needle, $haystack);
+
     if ($needle === "") {
         return false;
-    } else {
+    } elseif ($pos || $pos === 0) {
         return "Found '" . $needle . "' at index " . $pos;
     }
 }
