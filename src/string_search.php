f374be27eb101e37799e45e56c39bc4d004501e6
Rick Gates <rickgates@outlook.com>
2016-06-21 19:31:22 -0400

    Code challenge three changes.

---
+++ src/string_search.php
@@ -0,0 +1,16 @@
+<?php
+	//$haystack = "intermittent";
+	//$needle = "matt";
+	//$needle = "";
+	
+	if($needle !== "") {
+	$pos = strpos(strtolower($haystack),strtolower($needle));
+	if($pos === false) {
+			echo "'$needle' not found."; 
+		} else {
+			echo "Found '$needle' at position $pos.";
+		}
+	} else {
+		echo "No substring data exists.";
+	}
+?>
