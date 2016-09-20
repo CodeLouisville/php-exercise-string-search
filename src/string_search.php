92df3ffc5324ad0f7c726d3fe5ea429c351c1945
jnels <s0ckeyeus@yahoo.com>
2016-09-20 18:38:33 -0400

    2nd try after reading directions

--- src/string_search.php
+++ src/string_search.php
@@ -2,10 +2,10 @@
 
 function string_search($haystack, $needle) {
         $search = strpos($haystack, $needle);
-
-        if ($search) {
+  
+        if (is_int($search)) {
             return "Found '$needle' at index $search";
         } else {
-            return "false";
+            return false;
         }
 }
