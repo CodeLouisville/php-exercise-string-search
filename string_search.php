2131c114825a8deb0e548e792f10cb766e56e400
Schuyler Grisham <Schuyler Grisham>
2016-09-20 19:21:07 -0400

    attempt 4 at code challenge

--- string_search.php
+++ string_search.php
@@ -1,19 +1,10 @@
 <?php
 function string_search($needle, $haystack){
-  if(empty($needle)){
+  if(empty($needle) || !strstr($haystack, $needle)){
     return false;
   } else {
     $position = strpos($haystack, $needle);
-
-    if($position || $needle === '') {
-        return false;
-    } else {
-        return "Found '$needle' at index $position";
-  }
-  if ($position !== false) {
     return "Found '$needle' at index $position";
   }
-  return false;
-}
 }
 ?>
