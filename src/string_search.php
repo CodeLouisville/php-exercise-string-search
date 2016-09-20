a6201dd70eed927bb52e5cd14638a6ee39309af4
Kathryn <kanne1126@gmail.com>
2016-09-20 18:17:04 -0400

    echo changed to return

--- src/string_search.php
+++ src/string_search.php
@@ -5,13 +5,13 @@ function string_search($haystack, $needle){
 
   if (!$haystack || !$needle) {
     return false;
-  } elseif ($position == 0){
-    echo "'Found '$needle' at index $position'";
-    }  elseif (!$position){
+  }elseif ($position == 0){
+      return "Found '$needle' at index $position";
+  }elseif (!$position){
          return false;
-      }  else {
-          echo "'Found '$needle' at index $position'";
+  }else {
+          return "Found '$needle' at index $position";
          }
 }
-string_search("Some string", "string");
+
  ?>
