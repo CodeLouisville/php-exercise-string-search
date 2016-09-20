c94880d9272f6d33819ba3b6db66512a188717c0
Kathryn <kanne1126@gmail.com>
2016-09-19 22:52:34 -0400

    null fixed

--- src/string_search.php
+++ src/string_search.php
@@ -3,15 +3,14 @@ function string_search($haystack, $needle){
 
   $position =  strpos($haystack, $needle);
 
-  if ($position == 0){
-    echo "Found '$needle' at position $position";
-  }  elseif (!$position){
+  if (!$haystack || !$needle) {
     return false;
-     }  else {
-          echo "Found '$needle' at position $position";
-      }
+  } elseif ($position == 0){
+    echo "Found '$needle' at index $position";
+    }  elseif (!$position){
+         return false;
+      }  else {
+          echo "Found '$needle' at index $position";
+         }
 }
-
-
-
  ?>
