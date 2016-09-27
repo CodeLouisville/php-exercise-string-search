c436ba8b8a6288c9ef187f3e3001a3402c8d0773
jnels <s0ckeyeus@yahoo.com>
2016-09-27 19:09:38 -0400

    Testing 1, 2, 3

--- src/string_search.php
+++ src/string_search.php
@@ -3,10 +3,10 @@
 function string_search($needle, $haystack) {
         if ($needle === "") {
             return false;
-        } else {
-            $search = strpos($haystack, $needle);
-        }
-  
+        } 
+            
+        $search = strpos($haystack, $needle);
+
         if (is_int($search)) {
             return "Found '$needle' at index $search";
         } else {
